fc8bb3c17a3cf27e2d4672b31279b0b4c4a0312c
Taylor Casse <taylorcasse@gmail.com>
2022-10-25 13:07:24 +0200

    Started with collision detection

--- src/map.js
+++ src/map.js
@@ -1,12 +1,12 @@
 import {ctx} from '../index.js';
 
-
 export class Map {
-    constructor({ position, imgPath}) {
+    constructor({ position, imgPath, layers}) {
         this.position = position;
         this.imgPath = imgPath;
         this.img = new Image();
         this.img.src = imgPath;
+        this.layers = layers;
     }
 
     draw () {
