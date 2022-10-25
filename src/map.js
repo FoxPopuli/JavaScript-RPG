ab117336c11f6c7788892b86fe164cfdac26be8b
Fox <taylorcasse@gmail.com>
2022-10-25 15:47:09 +0200

    Imported The Clearing json map file and extracted collision layer

--- src/map.js
+++ src/map.js
@@ -1,16 +1,36 @@
 import {ctx} from '../index.js';
 
 export class Map {
-    constructor({ position, imgPath, layers}) {
+    constructor({ position, imgPath, mapFile}) {
         this.position = position;
         this.imgPath = imgPath;
         this.img = new Image();
         this.img.src = imgPath;
-        this.layers = layers;
+        this.mapFile = mapFile;
+        
+        this.collisionArr = this.mapFile.layers.find(obj => {
+            return obj.name === 'collision-tiles';
+        })
+
+
+        this.colMat = this.toMatrix(this.collisionArr.data)
+
+
     }
 
     draw () {
         ctx.drawImage(this.img, this.position.x, this.position.y);
+    }
+
+    toMatrix = function (array) {
+        let mapWidth = this.mapFile.width;
+        let matrix = [];
+        
+        for (let i = 0; i < array.length; i += mapWidth) {
+            matrix.push(array.slice(i, mapWidth + i));
+        }
+
+        return matrix;
     }
 
 }
